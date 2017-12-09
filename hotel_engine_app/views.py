8782a65a6cd1d3cc8abd1f3301602502a616358c
Narmin <nsalahli2019@ada.edu.az>
2017-12-09 17:43:00 +0400

    data from view to template

--- hotel_engine_app/views.py
+++ hotel_engine_app/views.py
@@ -1,22 +1,32 @@
 from django.shortcuts import render
 from django.views.generic import View
-from django.http import HttpResponse
+from .models import Hotel as HotelModel
+from django.http import HttpResponseNotFound
 # Create your views here.
 
 
 class Home(View):
     def get(self, request):
-        if request.method == 'GET':
-            return render(request, "home.html")
+        return render(request, "home.html", {'title': 'Top Hotels from view!'})
 
 
 class Register(View):
-    pass
+    def get(self, request):
+        return render(request, "register.html")
+
 
 
 class Login(View):
-    pass
+    def get(self, request):
+        return render(request, "login.html")
 
 
 class Hotel(View):
-    pass
+    def get(self, request, hotel_id):
+        try:
+            hotel = HotelModel.objects.get(pk=int(hotel_id))
+        except:
+            return HttpResponseNotFound()
+        else:
+            return render(request, "hotel.html", {'hotel':hotel})
+
